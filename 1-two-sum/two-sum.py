25425bdd3272b2ddb2e8b243ea048ce6a6da6d04
Jaewon Kim <124026092+CanDoJaewon@users.noreply.github.com>
2026-01-01 16:52:36 -0800

    Time: 3 ms (50.37%) | Memory: 18.8 MB (52.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[j] == target - nums[i]:
-                    return [i, j]
-        return []
-            
+        numMap = {}
+
+        for i, n in enumerate(nums):
+            # 1. Make a variable to find matching index
+            diff = target - n
+            # 2. if it found in numMap, it means we found pair. Return their index
+            if diff in numMap:
+                return [numMap[diff], i]
+
+            # 3. If it's not found in numMap, store the current number in numMap.
+            numMap[n] = i
